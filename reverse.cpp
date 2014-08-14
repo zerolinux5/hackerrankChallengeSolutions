f68798e0e299e3b34486414ba5d19c0c8214e6f8
Jesus Magana <maganazero5@gmail.com>
2014-08-14 11:33:28 -0700

    Works but is too slow

--- reverse.cpp
+++ reverse.cpp
@@ -2,11 +2,40 @@
 
 using namespace std;
 
-int main(){
+void reverse(int *array, int size, int comparison){
+	for(int n = 0; n < size; n++){
+		int j = size - 1;
+		int i = n;
+		while(i < j){
+			int temp = array[i];
+			array[i] = array[j];
+			array[j] = temp;
+			i++;
+			j--;
+		}
+	}
+
+	for(int i = 0; i < size;i++){
+		if(array[i] == comparison){
+			cout << i << endl;
+		break;
+		}
+	}
+}
+
+int main()
+{
 	int testNum;
 	cin >> testNum;
 	while(testNum--){
-
+		int size, num;
+		cin >> size >> num;
+		int array[size];
+		for(int i = 0; i < size;i++){
+			array[i] = i;
+		}
+		reverse(array, size, num);
 	}
+
 	return 0;
 }
